3e2eead7d350071b218f67ef9165bba013df2977
Ditract <hgsanez17@gmail.com>
2025-09-25 22:57:58 -0500

    feat: add checkout template, update styles and fix shipping form field names

--- apps/orders/forms.py
+++ apps/orders/forms.py
@@ -2,21 +2,38 @@ from django import forms
 from apps.shipping.models import Address
 from apps.orders.models import Payment
 
+
 class AddressForm(forms.ModelForm):
     class Meta:
         model = Address
         exclude = ['user']
+        labels = {
+            "full_name": "Nombre completo",
+            "street": "Calle",
+            "city": "Ciudad",
+            "province": "Departamento/Provincia",
+            "country": "País",
+            "postal_code": "Código Postal",
+            "phone": "Teléfono",
+        }
+
 
 class PaymentForm(forms.ModelForm):
     class Meta:
         model = Payment
         fields = ['method']
+        labels = {
+            "method": "Método de pago",
+        }
 
 
 class ComprobanteForm(forms.Form):
-    method = forms.ChoiceField(choices=[
-        ('NEQUI', 'Nequi'),
-        ('DAVIPLATA', 'Daviplata'),
-        ('BANK', 'Transferencia Bancaria'),
-    ])
-    comprobante = forms.ImageField()
+    method = forms.ChoiceField(
+        label="Método de pago",
+        choices=[
+            ('NEQUI', 'Nequi'),
+            ('DAVIPLATA', 'Daviplata'),
+            ('BANK', 'Transferencia Bancaria'),
+        ]
+    )
+    comprobante = forms.ImageField(label="Subir comprobante")
